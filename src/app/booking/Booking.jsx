18ffacecab9f47a96721fcafff8efa0c35058d2d
Akbar <tillayevakbarshoh@gmail.com>
2024-11-01 12:30:18 +0500

    fixed booking token

--- src/app/booking/Booking.jsx
+++ src/app/booking/Booking.jsx
@@ -51,7 +51,7 @@ export default function BookingPage() {
 
   const confirmEmail = async (token) => {
     try {
-      const res = await fetch(`https://restaurant-booking-system-production.up.railway.app/api/v1/confirm/${token}`, {
+      const res = await fetch(` https://restaurant-ferro-soft.netlify.app/confirm.app/confirm/${token}`, {
         method: 'GET',
       });
 
