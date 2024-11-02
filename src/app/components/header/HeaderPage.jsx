ed0066a4bafcac63aa60415f2c6b8c90f56efc38
Akbar <tillayevakbarshoh@gmail.com>
2024-11-02 16:18:25 +0500

    fixed home/menu to /menu

--- src/app/components/header/HeaderPage.jsx
+++ src/app/components/header/HeaderPage.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 import NoneData from '../NoneData';
 
 export default function HeaderPage({ data }) {
-  const { layout, contacts } = data;
-
-  if (!layout || !contacts) {
+  if (!data) {
     return <NoneData />;
   }
 
+  const { header_image, name, id } = data;
+
   return (
-    <div className="header" style={{ backgroundImage: `url(${layout.header_image})` }}>
-      <div className="header-blok" key={contacts.id}>
+    <div className="header" style={{ backgroundImage: `url(${header_image})` }}>
+      <div className="header-blok" key={id}>
         <h1 className="header__h1-1">Вкусная еда вовремя</h1>
-        <h1 className="header__h1-2">{contacts.name}</h1>
+        <h1 className="header__h1-2">{name}</h1>
         <h2 className="header__h1-3">Мы привлекаем ценителей истинного вкуса!</h2>
         <div className="header-blok__container">
           <Link href="/menu">
--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import HeaderPage from './HeaderPage';
-import NoneData from '../NoneData';
 
 export default async function Header() {
-    try {
-        // Запрос к первому API
-        const res1 = await fetch('https://restaurant-booking-system-production.up.railway.app/api/v1/contacts');
-        if (!res1.ok) {
-            throw new Error('Ошибка при получении данных от Layout');
-        }
-        const layoutData = await res1.json();
+    // Получение данных с сервера
+    const res = await fetch('https://restaurant-booking-system-production.up.railway.app/api/v1/contacts');
 
-        // Запрос ко второму API
-        const res2 = await fetch('https://restaurant-booking-system-production.up.railway.app/api/v1/contacts');
-        if (!res2.ok) {
-            throw new Error('Ошибка при получении данных от Contacts');
-        }
-        const contactsData = await res2.json();
-
-        // Объединение данных и передача их в компонент HeaderPage
-        const combinedData = {
-            layout: layoutData[0],  // Например, если layoutData — массив, выбираем первый объект
-            contacts: contactsData[0], // Точно так же с данными contacts
-        };
-
-        return (
-            <div>
-                <HeaderPage data={combinedData} />
-            </div>
-        );
-    } catch (error) {
-        console.error('Ошибка при запросе данных:', error);
-        return <div> <NoneData /> </div>;
+    if (!res.ok) {
+        throw new Error('Ошибка при получении данных');
     }
+
+    const [data] = await res.json(); // Получаем первый объект из массива
+
+    return (
+        <div>
+            <HeaderPage data={data} />
+        </div>
+    );
 }
